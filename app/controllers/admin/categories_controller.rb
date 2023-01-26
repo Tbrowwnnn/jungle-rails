1f89be223820522b497aef300097cb1c63d8bb2a
Tyler Brown <tyler.brown@telus.com>
2023-01-26 10:14:11 -0700

    implement categories drop down with route setup

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,14 @@
 class Admin::CategoriesController < ApplicationController
 
-  def show
+  def index
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
   end
+
+  def new
+  end
+  
+  def create
+  end
+  
 end
